10d73ff5bd9a14e9d159c9d1b63e5b312c7198d5
JimenaAlexandraRuizCerna <jruiz@hiper.com.pe>
2021-05-23 23:57:46 -0500

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //api: 'https://app-tp2-api.herokuapp.com',
-  api: 'http://ec2-54-172-179-183.compute-1.amazonaws.com:8080'
+  api: 'https://app-tp2-api.herokuapp.com',
+  //api: 'http://ec2-54-172-179-183.compute-1.amazonaws.com:8080'
 };
