b69fbd5c8687680fa2a4b00a1b3421e3cabe9dc0
JimenaAlexandraRuizCerna <jruiz@hiper.com.pe>
2021-06-07 19:55:09 -0500

    link deploy aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  api: 'https://app-tp2-api.herokuapp.com',
+  //api: 'https://app-tp2-api.herokuapp.com',
+  api: 'http://localhost:8080',
   api_ia: 'https://app-tp2-ia.herokuapp.com'
   //api: 'http://ec2-54-172-179-183.compute-1.amazonaws.com:8080'
 };
